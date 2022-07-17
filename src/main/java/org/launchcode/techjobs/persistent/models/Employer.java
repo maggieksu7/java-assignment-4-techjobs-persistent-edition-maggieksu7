4f79898ca1064c4e9d73597fa2a3f7eb864c2bb4
maggiedotel <maggieksu7@gmail.com>
2022-07-17 16:27:11 -0500

    part 2 completed

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message = "Error: Required field.")
+    @Size(max = 75, message = "Error: Cannot be longer than 75 characters.")
+    private String location;
 
+    public Employer() {
+    }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
