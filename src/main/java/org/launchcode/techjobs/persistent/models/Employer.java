e2e2ca07f45261cdc0715f5d9f7af0c93536d0ad
maggiedotel <maggieksu7@gmail.com>
2022-07-17 20:34:37 -0500

    part 3 completed

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -2,14 +2,22 @@ package org.launchcode.techjobs.persistent.models;
 
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
     @NotBlank(message = "Error: Required field.")
     @Size(max = 75, message = "Error: Cannot be longer than 75 characters.")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {
     }
